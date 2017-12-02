c38d0bc5c0ec003a34b9ade2b9aba3f6db0b79ce
Edoardo-PC\Edoardo <edoardo.lenzi@studenti.unitn.it>
2017-12-02 23:12:53 +0100

    Solution refact (dotnet standard lib 2.0)

---
+++ SpazioDati.Dandelion.Examples/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using SpazioDati.Dandelion.Domain.Models;
+
+namespace SpazioDati.Dandelion.Examples
+{
+    class Program
+    {
+        public static string Url1 = "http://milano.repubblica.it/cronaca/2017/11/16/news/divorzio_berlusconi_veronica_lario-181256569/?ref=RHPPLF-BH-I0-C8-P2-S1.8-T2";
+        public static string Url2 = "https://xunit.github.io/docs/getting-started-desktop.html";
+        public static string Text = "ronaldo & ronaldinho";
+
+        public static string Text1 = " a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a aa a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a a Silvio Berlusconi Mario Monti Robert De Niro";
+        public static string Text2 = "a a a a a a a a  a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a  a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a  a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a aa a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a aa a a a a a a a  a a a a a a a a a a a a a a a a a a a Silvio Berlusconi Mario Monti Robert De Niro";
+        public static string Token = "41d8b2b068754a9287cc3aeab1d4f079";
+
+        public static void Main()
+        {
+            MainAsync().Wait();
+        }
+
+        public static async Task MainAsync()
+        {
+            /*
+            var entities1 = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() {Text = Text, HttpMethod = HttpMethod.Get });
+            Debug.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities1));
+            var entities2 = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Text = Text1, HttpMethod = HttpMethod.Get });
+            Debug.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities2));
+            var entities3 = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Text = Text1, HttpMethod = HttpMethod.Post });
+            Debug.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities3));
+            var entities4 = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Text = Text, HttpMethod = HttpMethod.Post });
+            Debug.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities4));
+            */
+            /*var entities5 = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Text = Text2, HttpMethod = HttpMethod.Get });
+            Debug.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities5));*/
+            var entities6 = await Business.DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Token = Token, Text = Text, HttpMethod = HttpMethod.Post });
+            Console.WriteLine("\n\n\n Response: \n\n\n" + JsonConvert.SerializeObject(entities6));
+            Console.Read();
+            //var response = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Include = new List<IncludeOption> { IncludeOption.image, IncludeOption.categories, IncludeOption.lod }, Url = Url1, Lang = LanguageOption.it });
+            /*response = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Url = Url1, Lang = LanguageOption.it });
+            response = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Url = Url2});
+            response = await DandelionUtils.GetEntitiesAsync(new EntityExtractionParameters() { Url = Url2, Text = Text });*/
+
+
+            //var response2 = await DandelionUtils.GetSimilaritiesAsync(new TextSimilarityParameters() { Url1 = Url1, Text1 = Text, Url2 = Url2 });
+
+        }
+    }
+}
+
---
+++ SpazioDati.Dandelion.Test/ValidationTests/SourceValidationTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using SpazioDati.Dandelion.Business.Services;
+using SpazioDati.Dandelion.Domain.Models;
+using Xunit;
+
+namespace SpazioDati.Dandelion.Test.ValidationTests
+{
+    public class SourceValidationTests 
+    {
+
+        [Theory]
+        [MemberData(nameof(GetSingleSourceParameters))]
+        public void Should_ThrowException_When_CallVerifySingleSourceWithWrongParameters(SourceParameters parameters, string message, string wrongParameter)
+        {
+            //Act & Assert
+
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => SourceValidationService.verifySingleSource(parameters));
+            if (String.IsNullOrEmpty(wrongParameter))
+            {
+                Assert.Equal(message, ex.Message);
+            }
+            else
+            {
+                Assert.Equal($"{message}{Environment.NewLine}Parameter name: {wrongParameter}", ex.Message);
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Should_ThrowException_When_CallWikisearchWithWrongTextSource(string Text)
+        {
+            //Arrange
+            var parameters = new WikisearchParameters { Token = "Token", Text = Text };
+
+            //Act & Assert
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => SourceValidationService.verifySingleSource(parameters));
+            Assert.Equal($"{ErrorMessages.WrongText}{Environment.NewLine}Parameter name: {ErrorMessages.Text}", ex.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetMultipleSourcesParameters))]
+        public void Should_ThrowException_When_CallVerifyMultipleSourcesWithWrongParameters(TextSimilarityParameters parameters, string message, string wrongParameter)
+        {
+            //Act & Assert
+            ArgumentException ex = Assert.Throws<ArgumentException>(() => SourceValidationService.verifyMultipleSources(parameters));
+            if (String.IsNullOrEmpty(wrongParameter))
+            {
+                Assert.Equal(message, ex.Message);
+            }
+            else
+            {
+                Assert.Equal($"{message}{Environment.NewLine}Parameter name: {wrongParameter}", ex.Message);
+            }
+        }
+
+        public static IEnumerable<object[]> GetMultipleSourcesParameters()
+        {
+            yield return new object[] { new TextSimilarityParameters { Token = "Token" }, ErrorMessages.WrongSources, "" };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", Text1 = "Text", Url1 = "https://www.google.it" }, ErrorMessages.WrongSource1, ErrorMessages.Url1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", Text1 = "Text", Html1 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource1, ErrorMessages.Html1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", Text1 = "Text", HtmlFragment1 = "<p> Html Text </p>" }, ErrorMessages.WrongSource1, ErrorMessages.HtmlFragment1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", Url1 = "https://www.google.it", Html1 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource1, ErrorMessages.Html1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", Url1 = "https://www.google.it", HtmlFragment1 = "<p> Html Text </p>" }, ErrorMessages.WrongSource1, ErrorMessages.HtmlFragment1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text2 = "Text", HtmlFragment1 = "<p> Html Text </p>", Html1 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource1, ErrorMessages.HtmlFragment1 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", Text2 = "Text", Url2 = "https://www.google.it" }, ErrorMessages.WrongSource2, ErrorMessages.Url2 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", Text2 = "Text", Html2 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource2, ErrorMessages.Html2 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", Text2 = "Text", HtmlFragment2 = "<p> Html Text </p>" }, ErrorMessages.WrongSource2, ErrorMessages.HtmlFragment2 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", Url2 = "https://www.google.it", Html2 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource2, ErrorMessages.Html2 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", Url2 = "https://www.google.it", HtmlFragment2 = "<p> Html Text </p>" }, ErrorMessages.WrongSource2, ErrorMessages.HtmlFragment2 };
+            yield return new object[] { new TextSimilarityParameters { Token = "Token", Text1 = "Text", HtmlFragment2 = "<p> Html Text </p>", Html2 = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.WrongSource2, ErrorMessages.HtmlFragment2 };
+        }
+
+        public static IEnumerable<object[]> GetSingleSourceParameters()  
+        {
+            yield return new object[] { new SourceParameters { Token = "Token" } , ErrorMessages.WrongSource, "" };
+            yield return new object[] { new SourceParameters { Token = "Token", Text = "Text", Url = "https://www.google.it" }, ErrorMessages.MultipleSources, ErrorMessages.Url };
+            yield return new object[] { new SourceParameters { Token = "Token", Text = "Text", Html = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.MultipleSources, ErrorMessages.Html };
+            yield return new object[] { new SourceParameters { Token = "Token", Text = "Text", HtmlFragment = "<p> Html Text </p>" }, ErrorMessages.MultipleSources, ErrorMessages.HtmlFragment };
+            yield return new object[] { new SourceParameters { Token = "Token", Url = "https://www.google.it", Html = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.MultipleSources, ErrorMessages.Html };
+            yield return new object[] { new SourceParameters { Token = "Token", Url = "https://www.google.it", HtmlFragment = "<p> Html Text </p>" }, ErrorMessages.MultipleSources, ErrorMessages.HtmlFragment };
+            yield return new object[] { new SourceParameters { Token = "Token", HtmlFragment = "<p> Html Text </p>", Html = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.MultipleSources, ErrorMessages.HtmlFragment };
+            yield return new object[] { new SourceParameters { Token = "Token", Text = "<p> Html Text </p>", Html = "<html><body><p>Html Text</p></body></html>" }, ErrorMessages.MultipleSources, ErrorMessages.Html };
+        }
+    }
+}
